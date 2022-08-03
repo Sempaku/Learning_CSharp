248f9a346aa97675266024d98ca2757d6f21cf46
Semyon <42116085+Sempaku@users.noreply.github.com>
2022-08-03 21:18:43 +0300

    -comms

--- Basics of C#/Reflection/Application of reflection and type research/Program.cs
+++ Basics of C#/Reflection/Application of reflection and type research/Program.cs
@@ -9,10 +9,6 @@ namespace Применение_рефлексии_и_исследование_т
         static void Main(string[] args)
         {
             //Получение всех компонентов типа
-            //Метод GetMembers() возвращает все доступные компоненты типа в виде объекта MemberInfo.
-            //Этот объект позволяет извлечь некоторую информацию о компоненте типа.
-            //В частности, некоторые его свойства
-
             Type myType = typeof(Person);
             foreach(MemberInfo member in myType.GetMembers())
                 Console.WriteLine($"{member.DeclaringType} {member.MemberType} {member.Name}");
@@ -21,11 +17,7 @@ namespace Применение_рефлексии_и_исследование_т
             Console.WriteLine("________________");
 
             //BindingFlags
-            //DeclaredOnly: получает только методы непосредственно данного класса, унаследованные методы не извлекаются
-            //Instance: получает только методы экземпляра
-            //NonPublic: извлекает не публичные методы
-            //Public: получает только публичные методы
-            //Static: получает только статические методы
+
 
             foreach (MemberInfo member in myType.GetMembers(BindingFlags.DeclaredOnly | BindingFlags.Instance
                                                 | BindingFlags.NonPublic | BindingFlags.Public))
@@ -38,10 +30,6 @@ namespace Применение_рефлексии_и_исследование_т
             MemberInfo[] print = myType.GetMember("Print", BindingFlags.Instance | BindingFlags.Public);
             foreach(MemberInfo member in print)
                 Console.WriteLine($"{member.MemberType} {member.Name}");
-
-
-
-
 
         }
     }
